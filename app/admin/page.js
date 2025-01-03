2f0e5e161b00e2339a7c7081d1aeff952f0d3610
aysobay212 <57311890+aysobay212@users.noreply.github.com>
2025-01-03 11:12:23 +1100

    Simple authentication

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,11 +1,29 @@
 import Main from "@/components/Main";
+import Dashboard from "@/components/Dashboard";
+import Login from "@/components/Login";
 
 export const metadata = {
     title: "Hello World · Admin",
   };
 
 export default function AdminPage () {
+    
+    const authStatus = false
+
+    // by default, renders login page
+    let children = (
+        <Login />
+    )
+
+    // overrides render to admin if authStatus
+    if (authStatus) {
+        children = <Dashboard />
+    }
+
+    // renders out the page
     return (
-        <Main>admin dashboard</Main>
+        <Main>
+            {children}
+        </Main>
     )
 }
--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
 import Main from "@/components/Main";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <Main>
-      landing page
+    <Main className="">
+      <Hero />
     </Main>
   );
 } 
